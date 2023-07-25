5c395e450d4bcf851b15a39789907485c9253326
Carlos Gonzalez <cegcdeveloper@gmail.com>
2023-07-24 20:14:45 -0600

    actualizacion de livewire a 2.12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -128,6 +128,8 @@
             }
         </style>
 
+        @wireUiScripts
+
         <!-- estilos para Select2 -->
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
         
